5b81961efcef31c2d35a9c8efac0ff3bf31ea57c
Claudeir <claudeirmota2@gmail.com>
2025-09-19 18:30:53 -0300

    Fix formatting and correct method name in Produto class

--- core/domain/entities/produtos.py
+++ core/domain/entities/produtos.py
@@ -1,12 +1,12 @@
 
 class Produto:
     def __init__(self, nome, preco, estoque, vendedor_id):
-        self.nome =nome
+        self.nome = nome
         self.preco = preco
         self.estoque = estoque
         self.vendedor_id = vendedor_id
     
-    def atualizr_estoque(self, quantidade):
-        if self.estoque + quantidade <0:
-            raise ValueError("Estoque insufciente")
+    def atualizar_estoque(self, quantidade):
+        if self.estoque + quantidade < 0:
+            raise ValueError("Estoque insuficiente")
         self.estoque += quantidade
