5e4fa53d6a65bdf6c683808390b532d120aebc54
Claudeir <claudeirmota2@gmail.com>
2025-09-19 18:19:47 -0300

    Refactor Produto class to define attributes and methods explicitly

--- core/domain/entities/produtos.py
+++ core/domain/entities/produtos.py
@@ -1,8 +1,12 @@
-from dataclasses import dataclass
 
-@dataclass
-class Produto:
-    nome: str
-    preco: float
-    estoque: int
-    vendedor_id: int
+class Produdo:
+    def __init__(self, nome, preco, estoque, vendedor_id):
+        self.nome =nome
+        self.preco = preco
+        self.estoque = estoque
+        self.vendedor_id = vendedor_id
+    
+    def atualizr_estoque(self, quantidade):
+        if self.estoque + quantidade <0:
+            raise ValueError("Estoque insufciente")
+        self.estoque += quantidade
